140cc9d0a61560159decc1e02ea44e7cd3307166
suleiman yusuf <108791588+hetrox8@users.noreply.github.com>
2023-09-28 00:11:13 +0300

    Update index.rb

--- index.rb
+++ index.rb
@@ -1,33 +1,59 @@
-# frozen_string_literal: true
-
-# Define a hash to map Morse code to characters
-MORSE_CODE = {
-  '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
-  '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
-  '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
-  '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
-  '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
-  '--..' => 'Z', '-----' => '0', '.----' => '1', '..---' => '2',
-  '...--' => '3', '....-' => '4', '.....' => '5', '-....' => '6',
-  '--...' => '7', '---..' => '8', '----.' => '9'
+CODES_CHAR = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
 }.freeze
 
-# Method to decode a Morse code character and return it in uppercase
-def decode_char(morse_char)
-  MORSE_CODE[morse_char] || ''
+def decode_char(val)
+  CODES_CHAR[val]
 end
 
-# Method to decode an entire word in Morse code and return the string representation
-def decode_word(morse_word)
-  morse_word.split.map { |char| decode_char(char) }.join
+char = decode_char('.-')
+puts char
+
+def decode_word(word)
+  letters = word.split
+  decoded_word = ''
+  letters.each do |letter|
+    decoded_char = decode_char(letter)
+    decoded_word += decoded_char unless decoded_char.nil?
+  end
+  decoded_word
 end
 
-# Method to decode the entire message in Morse code
-def decode(message)
-  message.split('   ').map { |word| decode_word(word) }.join(' ')
+puts decode_word('-- -.--')
+
+def decode(morse_text)
+  words = morse_text.split('   ')
+  decoded_text = ''
+  words.each do |word|
+    decoded_text += "#{decode_word(word)} "
+  end
+  decoded_text.strip
 end
 
-# Example usage to decode the message from the old bottle
-morse_message = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
-decoded_message = decode(morse_message)
-puts decoded_message  # Output: "ABOVE FULL OF RUBIES"
+puts decode('-- -.--   -. .- -- .')
+puts decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
